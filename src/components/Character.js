adc275264b2edde28a7e31aaebbc713ac9d04503
lowlyHero <e.nunn1009@gmail.com>
2019-06-06 10:42:01 -0400

    added styles to make list more readable

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,7 +3,7 @@ import React from "react";
 const Character = props => {
   return (
     <ul>
-  <li>Name : {props.character.name}</li>
+  <li><strong>Name: </strong>{props.character.name}</li>
   <li>Height(cm): {props.character.height}</li>
   <li>Hair Color: {props.character.hair_color}</li>
   <li>Eye Color: {props.character.eye_color}</li>
